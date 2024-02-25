ca778111520a9a0b20a69bdd24f89954d5b56510
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 01:19:19 +0530

    Update q1.java

--- q1.java
+++ q1.java
@@ -1,3 +1,11 @@
+QUESTION 1 :
+Write a java program to display following messages.
+Hello “ITERIAN”
+Welcome to Siksha ‘O’ Anusandhan Family.
+Welcome to “Introduction to Computer Laboratory”
+Java is fun for All !!
+
+    
 public class q1{
     public static void main(String[]args){
         System.out.println("Hello \"ITERIAN \"");
