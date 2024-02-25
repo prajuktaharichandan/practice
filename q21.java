ecf07af761ea8230c34f69404a27dfe69ca55bf3
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 01:04:54 +0530

    Update q21.java

--- q21.java
+++ q21.java
@@ -3,10 +3,14 @@ public class q21{
     public static void main(String[]args){
         double d,t,g;
         Scanner obj=new Scanner(System.in);
-        System.out.println("Enter the number of seconds");
-        t=obj.nextInt();
+        System.out.println("Enter the number of seconds : ");
+        t=obj.nextDouble();
         g=32.174;
         d=1.0/2*g*t*t;
-        System.out.println("Distance travelled" +d);
+        System.out.println("Distance travelled :" +d);
     }
 }
+
+OUTPUT
+Enter the number of seconds :    5.4
+Distance travelled : 469.09692
