1b0a6d503bc8cc7fec2ccc0cab4002e4dab92eb7
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-25 22:42:29 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,8 @@
+public class q1{
+    public static void main(String[]args){
+        System.out.println("Hello \"ITERIAN \"");
+        System.out.println("Welcome to Sikshya'O'Anusandhan Family");
+        System.out.println("Introduction to Computer Laboratory");
+        System.out.println("Java is fun for All!!");     
+    }
+}
