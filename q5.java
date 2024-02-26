21aa78a3bd99a44a789808d20aa2db489c61cdb6
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 15:51:55 +0530

    Update q5.java

--- q5.java
+++ q5.java
@@ -1,3 +1,7 @@
+Question 5 : Write a java program to greet someone. Your program stores person name and greeting
+message into two variables, and displays their values on the screen.
+E.g. Hey Rishi, Good Morning!
+
 public class q5{
     public static void main(String[]args){
         String name = "Virat";
@@ -5,3 +9,6 @@ public class q5{
         System.out.println(" Hey "+name+ "," +msg+"!");
     }
 }
+
+OUTPUT : 
+Hey Virat,How are you!
