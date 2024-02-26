70baa0649ff6e0aef8d966d19dd563080790c424
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 16:24:10 +0530

    Update q20.java

--- q20.java
+++ q20.java
@@ -1,3 +1,6 @@
+Question 20 : The distance between two cities (in km.) is input through the keyboard. Write a java program
+to convert and print this distance in meters, feet, inches and centimetres.
+
 import java.util.Scanner;
 public class q20{
     public static void main(String[]args){
@@ -9,9 +12,17 @@ public class q20{
         feet=km*3280.8399;
         inches=km*39370.0787;
         centimeter=km*100000;
-        System.out.println(km + "km is" +"  " +meter + "meter");
-        System.out.println(km + "km is" +"  " +feet +"feet");
-        System.out.println(km + "km is" +"  " +inches +"inch");
-        System.out.println(km + "km is" +"  " +centimeter +"centimeter");
+        System.out.println(km + " km is " +"  " + meter  +  " meter ");
+        System.out.println(km + " km is " +"  " + feet  +  " feet ");
+        System.out.println(km + " km is " +"  " + inches +  " inches ");
+        System.out.println(km + " km is " +"  " + centimeter +  " centimeter ");
     }
 }
+
+OUTPUT :
+Enter the distance in kilometer
+20
+20.0 km is   20000.0 meter 
+20.0 km is   65616.798 feet
+20.0 km is   787401.574 inches
+20.0 km is   2000000.0 centimeter
