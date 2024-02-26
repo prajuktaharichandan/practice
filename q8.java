50b09e45211e0d0d19c8277137ac8b701a3f3b1b
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 15:58:08 +0530

    Update q8.java

--- q8.java
+++ q8.java
@@ -1,3 +1,6 @@
+Question 8 : Write a java program to exchange the values of two variables of integer type X and Y without
+using third temporary variable.
+
 public class q8{
     public static void main(String[]args){
         int x=50;
@@ -7,5 +10,13 @@ public class q8{
         y=x-y;
         x=x-y;
         System.out.println("After Swapping\n" +" x="+ x +"\n y=" + y);
-        } 
+    } 
 }
+
+OUTPUT : 
+Before Swapping
+ x=50
+ y=30
+After Swapping
+ x=30
+ y=50
