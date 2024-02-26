12623828206986df99bdecbf2ff76f287a93daae
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 15:59:48 +0530

    Update q9.java

--- q9.java
+++ q9.java
@@ -1,3 +1,11 @@
+Question 9 : What do each of the following print?
+//a)System.out.println(5);
+//b)System.out.println((float)25/6);
+//c)System.out.println(5+'6');
+//d)System.out.println(5+7+'9');
+//e)System.out.println("92"+7+5);
+//f)System.out.println(2+"9");
+
 public class q9{
     public static void main(String[]args){
         System.out.println(5);
@@ -8,3 +16,11 @@ public class q9{
         System.out.println(2+"9");
     }
 }
+
+OUTPUT : 
+5
+4.1666665
+59
+69
+9275
+29
