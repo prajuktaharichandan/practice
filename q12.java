84cd36ef7c0a1f617e7ef5e222c3353c0b4e1085
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 16:03:59 +0530

    Update q12.java

--- q12.java
+++ q12.java
@@ -1,3 +1,8 @@
+Question 12 : What do each of the following print?
+//a)System.out.println('b');
+//b)System.out.println('b' + 'c');
+//c)System.out.println((char) ('a' + 4));
+
 public class q12{
     public static void main(String[]args){
         System.out.println('b');
@@ -5,3 +10,8 @@ public class q12{
         System.out.println((char)('a'+4));
     } 
 }
+
+OUTPUT : 
+b
+197
+e
