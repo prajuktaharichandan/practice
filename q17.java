2b2f1ed36d4247ef68a70c48925279a7780d1b6a
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 16:14:34 +0530

    Update q17.java

--- q17.java
+++ q17.java
@@ -1,3 +1,12 @@
+Question 17 : Enter the basic salary of an employee of an organization through the keyboard. His dearness
+allowance (DA) is 40% of basic salary, and house rent allowance (HRA) is 20% of basic
+salary. Write a java program to calculate his gross salary. Print the DA, HRA and Gross salary.
+Here is the sample run:
+Enter basic salary: 15600
+DA is 6240.0
+HRA is 3120.0
+Gross salary is 24960
+
 import java.util.Scanner;
 public class q17{
     public static void main(String[]args){
@@ -13,3 +22,10 @@ public class q17{
         System.out.println("GROSS is "+GROSS);
     }
 }
+
+OUTPUT : 
+Enter the basic salary
+20000
+DA is: 8000.0
+HRA is: 4000.0
+GROSS is 32000.0
