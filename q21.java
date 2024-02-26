da83eda590e287d0c9305ba32263a83c1756fc0d
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 16:25:30 +0530

    Update q21.java

--- q21.java
+++ q21.java
@@ -1,3 +1,9 @@
+Question 21 : When a brick is dropped from a tower, it falls faster and faster until it hits the earth. The
+distance it travels is given by d = (1/2) gt2
+Here d is in feet, t is the time in seconds, and g is 32.174.
+Write a program that asks the user for the number of seconds and then prints out the distance
+travelled.
+
 import java.util.Scanner;
 public class q21{
     public static void main(String[]args){
