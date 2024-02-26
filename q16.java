f403c38eebcf40d7089a58d9f8e2d35ba94d2b17
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 16:11:54 +0530

    Update q16.java

--- q16.java
+++ q16.java
@@ -1,3 +1,11 @@
+Question 16 : Write a java program that reads a Fahrenheit degree in a double value from the console, then
+converts it to Celsius and displays the result. The formula for the conversion is as follows:
+Celsius = (Fahrenheit-32) *(5/9)
+//Hint: Hint: In Java, 5 / 9 is 0, but 5.0 / 9 is 0.556
+Here is a sample run:
+Enter a degree in Fahrenheit: 54
+54 Fahrenheit is 12.22 Celsius
+
 import java.util.Scanner;
 public class q16{
     public static void main(String[]args){
@@ -8,3 +16,8 @@ public class q16{
         System.out.println( f + "Farenhite is" + c + "Celcius");
     }
 }
+
+OUTPUT : 
+Enter the degree in Farenhite:
+44
+44.0 Farenhite is 6.666666666666667 Celcius 
