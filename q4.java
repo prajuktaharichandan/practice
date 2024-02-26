3c8380cf51da697d243a3596e81d526cac88575b
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 15:50:31 +0530

    Update q4.java

--- q4.java
+++ q4.java
@@ -1,5 +1,12 @@
+Question 4: Write a java program that will display your First, Middle and Last name in different lines
+without using the method println( ).
+
 public class q4{
     public static void main(String[]args){
         System.out.println("Prajukta \nHarichandan");
     }
 }
+
+OUTPUT : 
+Prajukta 
+Harichandan
