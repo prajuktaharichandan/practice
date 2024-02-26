ba87a110339410171debdcb83dbd66eea60814dc
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 15:53:51 +0530

    Update q6.java

--- q6.java
+++ q6.java
@@ -1,3 +1,8 @@
+Question 6: Write a java program to store your Bank account no, name and balance in three different
+variables and display their value on the screen as given:
+“My name is Amit Kumar bearing account number 123456
+having balance 7654.98”
+
 public class q6{
     public static void main(String[]args){
         int acno = 34456;
@@ -6,3 +11,6 @@ public class q6{
         System.out.println("\"My name is" +" "+ name+" "+ "bearing account number"+ " "+ acno +" " +"having balance" +" " + bal);
     } 
 } 
+
+OUTPUT : 
+"My name is Prajukta bearing account number 34456 having balance 55133.33
