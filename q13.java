149195f7a83cbf07869dc4bb0363b131a104490e
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 16:05:09 +0530

    Update q13.java

--- q13.java
+++ q13.java
@@ -1,3 +1,12 @@
+Question 13 : Suppose that a variable a is declared as int a = 2147483647 (or equivalently,
+//Integer.MAX_VALUE ). What do each of the following print?
+//a)System.out.println(a);
+//b)System.out.println(a+1);
+//c)System.out.println(2-a);
+//d)System.out.println(-2-a)
+//e)System.out.println(2*a);
+//f)System.out.println(4*a);
+
 public class q13{
     public static void main(String[]args){
         int a=2147483647;
@@ -8,4 +17,12 @@ public class q13{
         System.out.println(2*a);
         System.out.println(4*a);
     }
-}        
+}      
+
+OUTPUT : 
+2147483647
+-2147483648
+-2147483645
+2147483647
+-2
+-4
