f730b6cbd85fc35a592d5b9ad8cc084bf17eb5cf
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 16:18:58 +0530

    Update q19.java

--- q19.java
+++ q19.java
@@ -1,3 +1,9 @@
+Question 19 : Write a java program that reads an integer between 0 and 1000 and adds all the digits in the
+integer. For example, if an integer is 749, the sum of all its digits is 20.
+Hint: Use the % operator to extract digits, and use the /
+operator to remove the extracted digit.
+For instance, 749 % 10 = 9 and 749 / 10 = 74. 
+
 import java.util.Scanner;
 public class q19{
     public static void main(String[]args){
@@ -15,3 +21,9 @@ public class q19{
         System.out.println("the sum of the digit is" +sum);
     }
 } 
+
+OUTPUT : 
+Enter a number between 0 to 1000
+999
+the sum of the digit is: 27
+    
