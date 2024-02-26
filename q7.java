c48dc4d46167d694fd075c060fc3599b92dac837
Prajukta Harichandan <149325792+prajuktaharichandan@users.noreply.github.com>
2024-02-26 15:56:02 +0530

    Update q7.java

--- q7.java
+++ q7.java
@@ -1,3 +1,6 @@
+Question 7: Write a java program to exchange the values of two variables of integer type X and Y using
+third temporary variable Z
+
 public class q7{
     public static void main(String[]args){
         int x=20;
@@ -9,3 +12,11 @@ public class q7{
         System.out.println("After swapping\n" +"x="+x +"\ny="+y);       
     }
 }
+
+OUTPUT : 
+Before swapping
+x=20
+y=40
+After swapping
+x=40
+y=20
